718d4635f814ddbc9014fca8da924c833c42c401
Eric Rosário <ericsantos.r@hotmail.com>
2019-02-15 00:39:21 -0300

    Novos estilos para a aplicacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  texto = 'Bem Vindo a Salvador -BA';
+  texto = 'Bem Vindo a Salvador - BA';
   lat = -12.9711100;
   long = -38.5108300;
   zoom = 15;
